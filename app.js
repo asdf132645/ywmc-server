1b66a83c5a17e36dc79b499d20f8f9faefd56b08
coin <amber132623@gmail.com>
2024-11-19 15:25:55 +0900

    페이로드 요청 제한 푸는 코드 추가완료

--- app.js
+++ app.js
@@ -14,9 +14,8 @@ const connectionString = "Driver={Adaptive Server Enterprise};Server=128.9.2.30;
 
 
 app.use(express.json());
-app.use(express.json({ limit: '300mb' })); // 요청 크기 제한 설정
-app.use(express.urlencoded({ limit: '300mb', extended: true }));
-
+app.use(express.json({ limit: '1000mb' })); // JSON 요청 크기 제한 1GB로 설정
+app.use(express.urlencoded({ limit: '1000mb', extended: true })); // URL-encoded 데이터 크기 제한
 
 // 연결 함수
 async function connectToDatabase() {
